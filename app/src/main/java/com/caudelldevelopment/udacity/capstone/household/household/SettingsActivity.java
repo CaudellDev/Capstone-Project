f36d768241139e613907db88292684fcca888fc2
Tyler Caudell <caudell.tyler@gmail.com>
2018-04-25 21:04:18 -0500

    SettingsActivity now has a title and a back arrow.

--- app/src/main/java/com/caudelldevelopment/udacity/capstone/household/household/SettingsActivity.java
+++ app/src/main/java/com/caudelldevelopment/udacity/capstone/household/household/SettingsActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity implements SettingsFragment.OnSettingsFragListener {
 
@@ -13,6 +15,10 @@ public class SettingsActivity extends AppCompatActivity implements SettingsFragm
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getSupportFragmentManager().beginTransaction()
                                     .replace(R.id.settings_view_holder, SettingsFragment.getInstance(), "settings_fragment")
@@ -25,5 +31,18 @@ public class SettingsActivity extends AppCompatActivity implements SettingsFragm
         data.putExtra(SIGN_OUT_CLICKED, true);
         setResult(Activity.RESULT_OK, data);
         finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // For some reason, without this manual finish on back
+            // MainActivity gets recreated causing a NullPointerException
+            // on mUser.
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
